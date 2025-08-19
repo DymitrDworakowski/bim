9b13605bee575024639427970ceaf359b51e24fe
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2025-08-19 14:18:56 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-
-import './App.css';
-import NavBar from "./components/NavBar.jsx"
-import Home from "./components/Home.jsx"
-import Modal from "./components/Modal.jsx"
-import CourseList from "./components/CoursesList.jsx"
+import "./App.css";
+import NavBar from "./components/NavBar.jsx";
+import Home from "./components/Home.jsx";
+import Modal from "./components/Modal.jsx";
+import CourseList from "./components/CoursesList.jsx";
 function App() {
   return (
     <div className="App">
-      <NavBar/>
-        <Home/>
-        <Modal/>
-        <CourseList/>
+      <NavBar />
+      <Home />
+      <Modal />
+      <CourseList />
     </div>
   );
 }
