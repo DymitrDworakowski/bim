27603a43d39f6c6d4098b3e5dceaf277396e0c74
DymitrDworakowski <116100679+DymitrDworakowski@users.noreply.github.com>
2025-08-19 14:18:45 +0200

    navbar add

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+function NavBar() {
+  return (
+    <ul>
+      <button>Strona główna</button>
+      <button>Szkolenia</button>
+      <button>Case study</button>
+      <button>Blog</button>
+    </ul>
+  );
+}
+
+export default NavBar;
